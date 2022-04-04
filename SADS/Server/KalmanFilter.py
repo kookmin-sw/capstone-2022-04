2a15ffed1bf5b1ccdd78785485736a5e71f0fb73
Yong-Jun <n0y0j77@gmail.com>
2022-04-04 20:59:29 +0900

    Upload Kalman Filter Class

---
+++ SADS/Server/KalmanFilter.py
@@ -0,0 +1,30 @@
+'''
+applyFilter(): 불안정한 RSSI를 평활화
+'''
+class KalmanFilter():
+    def __init__(self, processNoise, measurementNoise):
+        super(KalmanFilter, self).__init__()
+        self.processNoise = processNoise # Process noise = 0.0005
+        self.measurementNoise = measurementNoise # Measurement noise = 20
+        self.estimatedRSSI = 0.0 # calculated rssi
+        self.errorCovarianceRSSI = 0.0 # calculated covariance
+        self.isInitialized = False # initialization flag
+
+    def applyFilter(self, rssi):
+        priorRSSI = 0.0
+        kalmanGain = 0.0
+        priorErrorCovarianceRSSI = 0.0
+
+        if not self.isInitialized:
+            priorRSSI = rssi
+            priorErrorCovarianceRSSI = 1
+            self.isInitialized = True
+        else:
+            priorRSSI = self.estimatedRSSI
+            priorErrorCovarianceRSSI = self.errorCovarianceRSSI + self.processNoise
+
+        kalmanGain = priorErrorCovarianceRSSI / (priorErrorCovarianceRSSI + self.measurementNoise)
+        self.estimatedRSSI = priorRSSI + (kalmanGain * (rssi - priorRSSI))
+        self.errorCovarianceRSSI = (1 - kalmanGain) * priorErrorCovarianceRSSI
+
+        return self.estimatedRSSI
