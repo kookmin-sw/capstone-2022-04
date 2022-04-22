658eff6276f6af6469da1979279a86b973f9df3b
Yong-Jun <n0y0j77@gmail.com>
2022-04-22 20:15:44 +0900

    Upload Firebase class in server

---
+++ SADS/Server/Firebase.py
@@ -0,0 +1,56 @@
+from firebase_admin import firestore
+from firebase_admin import messaging
+
+class Firebase():
+    def __init__(self):
+        self.db = firestore.client()
+    
+    def sendMessage(self, cnt):
+        registration_token = 'cjCeV5oSRnuhyF8VljAEtZ:APA91bENgw7naurauGXZndZnei1xtss1mDepQd7_gmSBnzHCfu-m4qBkTKr8Cw-OsGoMtNUNrpOyIN9DqYPwadtN_UPgiDaJozT0gAiLSSAP1VMl78-S40Q9um58z-_Sj3fBvPwgjYZo'
+
+        message = messaging.Message(
+        notification=messaging.Notification(
+            title='Detect Spoofing Attack',
+            body=str(cnt),
+        ),
+        token=registration_token,
+        )
+
+        response = messaging.send(message)
+        # Response is a message ID string.
+        print('Successfully sent message:', response)
+    
+    def getPublicKey(self):
+        ref = self.db.collection(u'public').document(u'data')
+        return ref.get()
+        
+    def setEncryptionData(self, uuid, en):
+        doc_ref = self.db.collection(u'encry').document(u'data')
+        doc_ref.set({
+            u'uuid': uuid,
+            u'y1': en[0],
+            u'y2': en[1]
+        })
+
+    def setFlag(self):
+        doc_ref = self.db.collection(u'flag').document(u'data')
+        doc_ref.set({
+            u'flag': True,
+        })
+    
+    def deleteEncryptionData(self):
+        self.db.collection(u'encry').document(u'data').delete()
+
+    # def setSpoofingData(self, time, uuid, major, minor, rssi, mac):
+    #     epochTime = float(time)
+    #     cTime = datetime.datetime.fromtimestamp(epochTime)
+        
+    #     doc_ref = self.db.collection(u'spoofing').document(str(cTime))
+    #     doc_ref.set({
+    #         u'time': cTime,
+    #         u'uuid': uuid,
+    #         u'major': major,
+    #         u'minor': minor,
+    #         u'rssi': rssi,
+    #         u'mac': mac
+    #     })
