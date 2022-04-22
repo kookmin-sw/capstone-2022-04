7e06545506475e85e957fbb59115b1204e2e6851
Yong-Jun <n0y0j77@gmail.com>
2022-04-22 20:15:28 +0900

    Upload GaloisField class in server

---
+++ SADS/Server/GaloisField.py
@@ -0,0 +1,26 @@
+class GaloisField():
+    def __init__(self, p):
+        self.p = p
+        pass
+
+    def range(self):
+        point = []
+
+        for i in range(1, self.p):
+            a = self.cal(i)
+
+            for item in a:
+                point.append([i, item])
+        
+        return point
+        
+    #4777
+    def cal(self, x):
+        val = []
+        res = ( x**3 + x ) % self.p
+
+        for i in range(0, self.p):
+            if ( i ** 2 ) % self.p == res:
+                val.append(i)
+        
+        return val
