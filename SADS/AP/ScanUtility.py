0d11c9285bc84dc89571dc7ed2930c65b9a27945
Boyzmsc <boyzmsc@naver.com>
2022-05-02 14:52:56 +0900

    Update Unicast Phase using Random Time List (Anchor Point)

--- SADS/AP/ScanUtility.py
+++ SADS/AP/ScanUtility.py
@@ -26,7 +26,7 @@ def packetToString(packet):
     else:
         return ''.join('%02x' % struct.unpack("B", x)[0] for x in packet)
 
-def parse_events(sock, g_uuid, g_minor, g_major, loop_count = 100):
+def parse_events(sock, g_uuid, loop_count = 100):
     old_filter = sock.getsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, 14)
     flt = bluez.hci_filter_new()
     bluez.hci_filter_all_events(flt)
@@ -37,7 +37,7 @@ def parse_events(sock, g_uuid, g_minor, g_major, loop_count = 100):
         packet = sock.recv(255)
         dataString = packetToString(packet)
 
-        if len(dataString) > 83: 
+        if len(dataString) > 83:
             uuid = dataString[40:48] + "-" + dataString[48:52] + "-" + dataString[52:56] + "-" + dataString[56:60] + "-" + dataString[60:72]
             major = dataString[72:76]
             minor = dataString[76:80]
@@ -45,10 +45,8 @@ def parse_events(sock, g_uuid, g_minor, g_major, loop_count = 100):
             majorVal = int("".join(major.split()[::-1]), 16)
             minorVal = int("".join(minor.split()[::-1]), 16)
 
-            # Unicast Phase
-            # 고유 식별자인 Major와 Minor 값과 동일한 광고 패킷만을 Server에 송신
-            if (uuid == g_uuid) and (g_minor == str(minorVal)) and (g_major == str(majorVal)):
-               
+            if uuid == g_uuid:
+                
                 type = "iBeacon"
 
                 scrambledAddress = dataString[14:26]
@@ -59,7 +57,3 @@ def parse_events(sock, g_uuid, g_minor, g_major, loop_count = 100):
                 
                 resultsArray = [{"type": type, "time": time.time(), "uuid": uuid, "rssi": rssi, "macAddress": macAddress}]
                 return resultsArray
-              
-                
-
-
--- SADS/AP/udp-client.py
+++ SADS/AP/udp-client.py
@@ -2,18 +2,20 @@ import socket
 import threading
 import ScanUtility
 import bluetooth._bluetooth as bluez
+import random
+import time
 
 uuid = ''
-g_major = ''
-g_minor = ''
 client = None
+start_flag = False
+time_flag = True
 
 def rcvdBeacon():
         dev_id = 0
         global uuid
-        global g_major
-        global g_minor
         global client
+        global start_flag
+        global time_flag
         try:
             sock = bluez.hci_open_dev(dev_id)
             print ("\n *** Looking for BLE Beacons ***\n")
@@ -27,52 +29,70 @@ def rcvdBeacon():
         
         try:
             while True:
-                returnedList = ScanUtility.parse_events(sock, uuid, g_minor, g_major, 100)
+                returnedList = ScanUtility.parse_events(sock, uuid, 100)
                 if returnedList:
                     for item in returnedList:
-                        message = str(index) + ',' + str(item['time']) + ',' + item['uuid'] + ',' + str(item['rssi'])
-                        print(message)
-                        encodedMsg = message.encode(encoding="utf-8")
-                        client.sendall(encodedMsg)
-                        print("message sent!")
+                        if item == None:
+                            pass
+                        else:
+                            # Start Section
+                            if not start_flag:
+                                # 광고 패킷을 처음 수신 받았을 때
+                                timeTh = threading.Thread(target=setFlag, args=())
+                                timeTh.start()
+                                start_flag = True
+                            
+                            if start_flag and time_flag:
+                                # Unicast Phase
+                                # 광고 패킷을 수신받기 시작했고 (start_flag == True)
+                                # Promise Time일 때 (time_flag == True)
+                                # Server로 광고 패킷 송신
+                                message = str(index) + ',' + str(item['time']) + ',' + item['uuid'] + ',' + str(item['rssi'])
+                                print(message)
+                                encodedMsg = message.encode(encoding="utf-8")
+                                client.sendall(encodedMsg)
+                                print("message sent!")
                                 
                 index += 1
-                if flag:
-                    break
 
         except KeyboardInterrupt:
             pass
 
 def initSetting():
     global uuid
-    global g_major
-    global g_minor
     global client
 
     serverHost = '192.168.219.104'
     PORT = 9999
    
-    # Server로부터 UUID값과 고유 식별자인 Major, Minor 값 수신
+    # Server로부터 UUID값과 Random Seed Value 값 수신
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((serverHost, PORT))
     client.sendall("AP".encode())
 
     data = client.recv(1024)
     decodedData = data.decode("utf-8")
-
     splitData = decodedData.split(' ')
 
-    # Set UUID
+    # UUID 설정
     uuid = splitData[0]
     
-    # 고유 식별자인 Major와 Minor 값 설정
-    g_major, g_minor = int(splitData[1]), int(splitData[2])
-
+    # Random Seed Value에 따른 Seed 설정
+    random.seed(splitData[1])
     return True
 
+def setFlag():
+    global time_flag
+    
+    while True:
+        # Random Time List 요소 값 생성
+        time.sleep(random.randint(2,5))
+        
+        # Promise Time : 광고 패킷을 수신받는 시간 (time_flag == True)
+        # Non-promise Time : 광고 패킷을 수신받지 않는 시간 (time_flag == False)
+        time_flag = not time_flag
+        
 if __name__ == '__main__':
     if initSetting():
         rcvdBeaconTh = threading.Thread(target=rcvdBeacon, args=())
         rcvdBeaconTh.start()
-
-
